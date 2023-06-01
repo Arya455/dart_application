b7185642704576f3311a7d3c61b8d1251f5bb2af
Arya455 <aryasaseendran75@gmail.com>
2023-06-01 11:52:51 +0530

    initial commit

--- bin/collectionss/list_multiplesof2.dart
+++ bin/collectionss/list_multiplesof2.dart
@@ -1,1 +1,16 @@
+void main() {
+  List<int> list = [1, -3, 7, 9, 0, -6, 4, -2, 0, 10, -8, 5];
+ 
+  for (int index = 0; index < list.length; index++) {
+   
+      if (list[index] % 2 == 0) {
+       
+      
+        print(list[index]);
+
+      }
+    }
+   
+  }
+
 
